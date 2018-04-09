454d1d95b9796309043a4a0d5325c525b43cc488
sego92 <sego.maris@gmail.com>
2018-04-09 17:04:09 +0200

    app appareil created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MonPremierComponent } from './mon-premier/mon-premier.component';
+import { AppareilComponent } from './appareil/appareil.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MonPremierComponent
+    MonPremierComponent,
+    AppareilComponent
   ],
   imports: [
     BrowserModule
