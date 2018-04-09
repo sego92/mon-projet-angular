9c4e4bface37903dac55c0de004fdb21d7b25b8e
sego92 <sego.maris@gmail.com>
2018-04-09 10:47:26 +0200

    project created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { MonPremierComponent } from './mon-premier/mon-premier.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MonPremierComponent
   ],
   imports: [
     BrowserModule
